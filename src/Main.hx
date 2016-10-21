431e3f1daf7c871cc8d7a8ce2e44e61e8dc546aa
Aidan Lee <aidan.lee63@gmail.com>
2016-10-21 14:44:46 +0100

    added basic beginnings of the cli parser to replace current one.

--- src/Main.hx
+++ src/Main.hx
@@ -1,72 +1,16 @@
 package;
 
 import Sys.args;
-import src.FileHandler;
-import src.functions.*;
 import src.Const;
+import src.CLIParser;
 
 class Main
 {
     public static function main()
     {
         Const.setupStorage();
-        
-        var cliArgs:Array<String> = args();
-        if (cliArgs.length > 0)
-        {
-            switch (cliArgs[0])
-            {
-                case "update":
-                    var update = new Update();
-                    update.updatePackages();
-                    
-                case "upgrade":
-                    trace("Upgrade");
-                    
-                case "install":
-                    cliArgs.remove("install");
-                    var install = new Install(cliArgs);
 
-                case "remove":
-                    cliArgs.remove("remove");
-                    var remove = new Remove(cliArgs);
-                                    
-                case "add-repository":
-                    cliArgs.remove("add-repository");
-                    var ar:AddRepo = new AddRepo(cliArgs);
-                    
-                case "remove-repository":
-                    cliArgs.remove("remove-repository");
-                    
-                    var fh:FileHandler = new FileHandler();
-                    fh.removeRepository(cliArgs[0]);
-                    
-                case "create":
-                    var pkg:CreatePackage = new CreatePackage();
-
-                case "self-upgrade":
-                    trace("Self Upgrade");
-
-                case "find":
-                    trace("find");
-
-                case "list":
-                    var ls:ListPkgs = new ListPkgs();
-
-                case "help":
-                    trace("help");
-
-                case "version":
-                    Sys.println("GMR v0.1.0+0000000");
-                    
-                default:
-                    trace("no such command");
-            }
-        }
-        else
-        {
-            Sys.println("GameMaker:Studio Package Manager v0.1.0+0000000");
-            Sys.println("Type 'gmr help' for a list of commands and their usage");
-        }
+        var cli = new CLIParser(args());
+        cli.parseInput();
     }
 }
